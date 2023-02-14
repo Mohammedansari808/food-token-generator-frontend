162235f78a8f6e0457da87fdc12d9feff55366ba
Mohammedansari808 <mohammedansari808@gmail.com>
2023-02-14 13:12:26 +0530

    updated

--- src/Components/link.js
+++ src/Components/link.js
@@ -1,2 +1,3 @@
-export const fullLink = "https://food-token-generator-backend.onrender.com"
+export const fullLink = "http://localhost:4000"
 // http://localhost:4000
+//https://food-token-generator-backend.onrender.com
